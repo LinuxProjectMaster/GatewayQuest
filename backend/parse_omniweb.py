962a0dd611a1c8e4352e79bc669ca2c49c3dacf5
Ethan Schauder <111901310+LinuxProjectMaster@users.noreply.github.com>
2025-06-20 23:00:33 -0400

    Update parse_omniweb.py

--- backend/parse_omniweb.py
+++ backend/parse_omniweb.py
@@ -1,1 +1,19 @@
 
+import re
+import pandas as pd
+import requests
+
+url = "https://omniweb.gsfc.nasa.gov/staging/omni_goes_sis_brTsHNnLbr.lst"
+ascii_text = requests.get(url).text
+
+lines = []
+for line in ascii_text.split("\n"):
+    line = re.split(r"\s+", line)
+    lines.append(line[-1])
+
+columns = "flux".split()
+lines.pop(-1)
+df = pd.DataFrame(data=lines)
+omnidf = pd.read_csv("parsed_ace_epam.csv")
+df = pd.concat([df, omnidf], axis=1)
+df.to_csv("final_data.csv", index=False)

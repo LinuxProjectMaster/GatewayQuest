e33aa0434f107b657c3865127cde6719c50b9907
Ethan Schauder <111901310+LinuxProjectMaster@users.noreply.github.com>
2025-06-20 23:13:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import logo from './logo.svg';
 import './App.css';
+import Graph from "./Chart.js"
+import Home from "./Home.js"
+import AboutPage from "./About.js"
+import {Navbar, Nav, Col, Row, Container} from "react-bootstrap"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <div className="bg-dark text-white lead" style={{overflow: 'hidden' }}>
+          <Navbar bg="dark" variant="dark" expand="lg" style={{ borderBottom: "2px solid white"}}>
+                <Container>
+                    <Navbar.Brand href="/">Proton Projector</Navbar.Brand>
+                    <Navbar.Collapse>
+                        <Nav className="me-auto">
+                            <Nav.Link href="/about">About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/model" element={<Graph />} />
+                <Route path="/about" element={<AboutPage />} />
+               </Routes>
+            </Router>
+        </div>
   );
 }
 
